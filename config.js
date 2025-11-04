19000138bec058294f8d65870fddab6a4f2e12f4
Saffar333 <safinfargat2016@gmail.com>
2025-11-04 04:01:54 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,5 @@
+// Конфигурация Supabase
+const CONFIG = {
+    SUPABASE_URL: 'https://dxniagaevxtyexumfpsi.supabase.co',
+    SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR4bmlhZ2Fldnh0eWV4dW1mcHNpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjE3MzU2MzMsImV4cCI6MjA3NzMxMTYzM30.7uCkUQZrhAvNEyd7rKLL6E2vVLU-X06KgJtGP2K4gBo'
+};
